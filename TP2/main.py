149f214e5ff421e459551b989100f09803583eaa
marco novaes <marcool_98@hotmail.com>
2021-11-01 15:33:31 -0400

    add glouton

--- TP2/main.py
+++ TP2/main.py
@@ -37,10 +37,66 @@ def readMatrix():
 
 def branchBound():
     print("here")
+    
+#----------
+  
+
+def choixGlouton(c,nbDegre,voisins,n):
+    sommetsSansCouleur = [i for i in range(n) if c[i] == None]
+    listeCouleursVoisins=[]
+    for i in sommetsSansCouleur:
+        listeCouleursVoisins.append([c[i] for i in voisins[i]])
+    listeCouleursVoisins = [set(i) for i in listeCouleursVoisins]
+    for i in listeCouleursVoisins:
+        if None in i: i.remove(None)
+    nbCouleursVoisins = [len(a) for a in listeCouleursVoisins]
+    max_value = max(nbCouleursVoisins) 
+    indices = [index for index, element in enumerate(nbCouleursVoisins) if element == max_value]
+    if len(indices) == 1:
+        return sommetsSansCouleur[indices[0]] , listeCouleursVoisins[indices[0]]
+    else:
+        nbDegreDsatMax = [nbDegre[sommetsSansCouleur[i]] for i in indices]
+        max_value = max(nbDegreDsatMax)
+        v = nbDegreDsatMax.index(max_value)
+        return sommetsSansCouleur[indices[v]] , listeCouleursVoisins[v]
+        
+def minValue(couleursVoisins):
+    chosingColor = True
+    color = 0
+    while chosingColor:
+        if color in couleursVoisins:
+            color += 1
+        else:
+            chosingColor = False
+    return color
+
+    
+
+def glouton(m):
+#initialisation
+    n = len(m[0]) 
+    c = [None]*n
+    voisins = []
+    for i in range(n):
+        voisins.append([idx for idx in range(n) if m[i][idx]==1])
+    nbDegre = [len(i) for i in voisins] 
+    max_value = max(nbDegre)
+    v = nbDegre.index(max_value)
+    c[v] = 0
+    
+    while any(x == None for x in c):
+        v , couleursVoisins = choixGlouton(c,nbDegre,voisins,n)
+        c[v] = minValue(couleursVoisins)
+    print("Coloration: ",c)
+    
+    
+
+#----------
 
 def main():
     if algoType == "glouton": 
-    	print("glouton")
+        readMatrix()
+        glouton(graph)
     elif algoType=="branch_bound":
     	readMatrix()
     	branchBound()
