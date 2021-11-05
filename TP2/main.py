4f9dc6ecfb2c7e9a962e5b5193f169a9a057773b
marco novaes <marcool_98@hotmail.com>
2021-11-05 07:48:50 -0400

    add tabou

--- TP2/main.py
+++ TP2/main.py
@@ -3,6 +3,7 @@
 import argparse
 import numpy as np
 import time as tt
+from random import randrange
 
 #parsing args
 parser = argparse.ArgumentParser(description='parse main args')
@@ -23,7 +24,9 @@ showTime = args.time
 #global variables 
 time = 0
 graph = []
+aretes = []
 numberOfNodes = 0; 
+voisins = []
 
 def readMatrix(): 
     with open(pathToEx) as f:
@@ -52,7 +55,7 @@ def choixGlouton(c,nbDegre,voisins,n):
         nbDegreDsatMax = [nbDegre[sommetsSansCouleur[i]] for i in indices]
         max_value = max(nbDegreDsatMax)
         v = nbDegreDsatMax.index(max_value)
-        return sommetsSansCouleur[indices[v]] , listeCouleursVoisins[v]
+        return sommetsSansCouleur[indices[v]] , listeCouleursVoisins[indices[v]]
         
 def minValue(couleursVoisins):
     chosingColor = True
@@ -81,6 +84,7 @@ def glouton(m):
     while any(x == None for x in c):
         v , couleursVoisins = choixGlouton(c,nbDegre,voisins,n)
         c[v] = minValue(couleursVoisins)
+    print("\nnb couleur glouton :",max(c)+1)
     return c
     
     
@@ -133,6 +137,92 @@ def branchBound():
                     stack.append(i)
     return res
 
+def getNbConflit(c):
+    nbConflit = 0
+    for a,b in aretes:
+        if c[a] == c[b]:
+            nbConflit += 1
+    return nbConflit
+
+def tabou(c):
+    print("\nInit algo tabou \n")
+    #constantes
+    solutionSansConflit = c.copy()
+    alpha = 2
+    g = 10
+    critereArret = 5000 #à déterminer
+    n = len(graph[0]) 
+    
+    #variables d'interet
+    nbIter = 0
+    counterNoModif = 0
+    
+    listeTabou = [] #couple (noued,couleur,iterLimite)
+    
+    
+    #---------REDUCTION DE COULEUR
+    k = max(c) + 1 # nb couleurs utilisé dans c
+    for currentNoued in range(n):
+        if c[currentNoued] == k-1:
+            nbConflit = [] # nbConflit[0] = nb voisin de couleur 0
+            for j in range(k-1):                
+                nbConflit.append(len([voisin for voisin in voisins[currentNoued] if c[voisin] == j]))
+            minValue = min(nbConflit)
+            c[currentNoued] = nbConflit.index(minValue)
+    #---------
+    cStar = c.copy()
+    while counterNoModif < critereArret:
+        
+        #------ RECHERCHE TABOU
+        #generation de voisin
+        listeTabouReduite = [(el[0],el[1]) for el in listeTabou]
+        nbConflitMin = n*n #initialisation a grande valeur
+        for currentNoeud in range(n):
+            for i in range(max(c) + 1):
+                if i != c[currentNoeud] and (currentNoeud,i) not in listeTabouReduite: #check couleur legale
+                    candidat = c.copy()
+                    candidat[currentNoeud] = i
+                    conflits = getNbConflit(candidat)
+                    if conflits < nbConflitMin:
+                        #print("conflit minimal: ",conflits)
+                        nbConflitMin = conflits                    
+                        bestCandidat = [candidat,currentNoeud,c[currentNoeud]] #coloration, noeud modif, ancienne couleur
+                        if conflits == 0: #solution optimal
+                            print("PAS DE CONFLITS")
+                            print("candidat: ",candidat)
+                            tabou(candidat)
+                            return 
+                            
+                            
+                        
+        #actualisation solution courante
+        if nbConflitMin != n*n: #que faire si aucun candidat à cause de listetabou?
+            c = bestCandidat[0]
+            #print(c)
+        
+    
+                    
+        #MaJ listeTabou
+        l = alpha * nbConflitMin + randrange(0,g) + 1
+        if nbConflitMin != n*n:
+            listeTabou.append([bestCandidat[1],bestCandidat[2],l+nbIter])
+        listeTabou = [a for a in listeTabou if a[2] > nbIter]
+        #print("taille liste tabou", len(listeTabou))
+        #actualisation solution optimale
+        if getNbConflit(c) < getNbConflit(cStar):
+            cStar = c.copy()
+            counterNoModif = 0
+        else:
+            counterNoModif += 1
+        #print("counterNoModif ",counterNoModif)
+        #------
+        
+        nbIter += 1
+        
+    #print("solution tabou:         ",solutionSansConflit)
+    print("nb couleur algo tabou: ",max(solutionSansConflit)+1,"\n")
+    return(solutionSansConflit)
+
 def main():
     if algoType == "glouton": 
         readMatrix()
@@ -153,7 +243,21 @@ def main():
     	if showTime:
     	    print(t3 - t2)
     elif algoType == "tabou":
-	    print("tabou")
+        readMatrix()
+        t0 = tt.time()
+        n = len(graph[0])
+        for i in range(n):
+                for j in range (n):
+                    if i>j and graph[i][j] == 1:
+                        aretes.append((i,j))
+        for i in range(n):
+            voisins.append([idx for idx in range(n) if graph[i][idx]==1])
+        resultT = tabou(glouton(graph))
+        t1 = tt.time()
+        if showRes:
+            print(resultT)
+        if showTime:
+       	    print(t1 - t0)
     else: 
 	    print("this algorithm is not supported. please make sure to use 'glouton', 'branch_bound' or 'tabou'.")
 
